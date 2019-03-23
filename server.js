e0c213af3b73d94137e4108cd221b5f844241277
Bongani Khoza <bonganebnk@gmail.com>
2019-03-24 01:52:14 +0200

    Weather app for fun

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const request = require('request');
+
+const app = express();
+const apiKey = '40aacaa67a1c74c0e8e779225676bd47';
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//Set view engine
+app.set("view engine","ejs");
+
+app.get('/', function (req, res) {
+    res.render('index', {weather: null, error: null});
+});
+
+app.post('/', function (req, res) {
+    let city = req.body.city;
+    let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`
+
+    request(url, function (err, response, body) {
+        if(err){
+          res.render('index', {weather: null, error: 'Error, please try again'});
+        } else {
+          let weather = JSON.parse(body)
+          if(weather.main == undefined){
+            res.render('index', {weather: null, error: 'Error, please try again'});
+          } else {
+            let weatherText = `It's ${weather.main.temp} degrees in ${weather.name}!`;
+            res.render('index', {weather: weatherText, error: null});
+          }
+        }
+    });
+});
+
+app.listen(4000, function () {
+  console.log('Weather app listening on port 4000!')
+});
